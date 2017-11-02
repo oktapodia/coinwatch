d5301362318d24456cfb182dca9cbb8a9f99671e
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-11-03 00:25:59 +0100

    Add icons & improve autoupdater

--- app/main/AutoUpdater.js
+++ app/main/AutoUpdater.js
@@ -12,43 +12,29 @@ class AutoUpdater {
     this.autoUpdater.logger = log;
     this.autoUpdater.logger.transports.file.level = 'info';
 
-    this.autoUpdater.on('update-downloaded', this.constructor.onUpdateDownloaded);
     this.autoUpdater.on('checking-for-update', this.constructor.onCheckingForUpdate);
     this.autoUpdater.on('error', this.constructor.onError);
     this.autoUpdater.on('download-progress', this.constructor.onDownloadProgress);
+    this.autoUpdater.on('update-downloaded', ::this.onUpdateDownloaded);
     this.autoUpdater.on('update-available', ::this.onUpdateAvailable);
     this.autoUpdater.on('update-not-available', ::this.onUpdateNotAvailable);
 
     ipcMain.on('check-update', ::this.checkForUpdates);
   }
 
-  static onUpdateDownloaded(info) {
-    log.debug('onUpdateDownloaded');
-    log.debug('Update downloaded;');
-
-    log.debug(info);
-  }
-
   static onCheckingForUpdate() {
     log.debug('onCheckingForUpdate');
     log.debug('Checking for update...');
-
-    // this.autoUpdater.quitAndInstall();
   }
 
-  onUpdateAvailable(info) {
-    log.debug('onUpdateAvailable');
-    log.debug('Update available.');
-    log.debug(info);
-    this.displayUpdateAvailableDialog();
-  }
-
-  onUpdateNotAvailable(info) {
-    log.debug('onUpdateNotAvailable');
-    log.debug('Update not available.');
-    log.debug(info);
-
-    this.constructor.displayNoUpdateAvailableDialog();
+  static displayNoUpdateAvailableDialog() {
+    dialog.showMessageBox({
+      type: 'info',
+      buttons: ['Ok'],
+      title: 'No update available',
+      message:
+        'You already have the latest version available',
+    });
   }
 
   static onError(err) {
@@ -62,14 +48,57 @@ class AutoUpdater {
     log.debug(info);
   }
 
-  static displayNoUpdateAvailableDialog() {
+  onUpdateDownloaded(info) {
+    log.debug('onUpdateDownloaded');
+    log.debug('Update downloaded;');
+
+    log.debug(info);
+
+    this.displayQuitAndInstallDialog();
+  }
+
+  onUpdateAvailable(info) {
+    log.debug('onUpdateAvailable');
+    log.debug('Update available.');
+    log.debug(info);
+    this.constructor.displayUpdateAvailableDialog();
+  }
+
+  onUpdateNotAvailable(info) {
+    log.debug('onUpdateNotAvailable');
+    log.debug('Update not available.');
+    log.debug(info);
+
+    this.constructor.displayNoUpdateAvailableDialog();
+  }
+
+  static displayUpdateAvailableDialog() {
     dialog.showMessageBox({
       type: 'info',
-      buttons: ['Ok'],
-      title: 'No update available',
-      message:
-        'You already have the latest version available',
+      buttons: ['OK'],
+      title: 'Update Available',
+      message: 'Please wait until CoinWatch download the new version and ask you for install it.',
     });
+  }
+  displayQuitAndInstallDialog() {
+    dialog.showMessageBox(
+      {
+        type: 'question',
+        buttons: ['Update & Restart', 'Cancel'],
+        title: 'Update Available',
+        cancelId: 99,
+        message:
+          'New version ready to install. Would you like to update CoinWatch now?',
+      },
+      response => {
+        console.log(`Exit: ${response}`); // eslint-disable-line no-console
+
+        if (response === 0) {
+          this.setQuitState(true);
+          autoUpdater.quitAndInstall();
+        }
+      },
+    );
   }
 
   checkForUpdates() {
@@ -86,27 +115,6 @@ class AutoUpdater {
 
     log.debug('checkForUpdates triggered');
     this.autoUpdater.checkForUpdates();
-  }
-
-  displayUpdateAvailableDialog() {
-    dialog.showMessageBox(
-      {
-        type: 'question',
-        buttons: ['Update & Restart', 'Cancel'],
-        title: 'Update Available',
-        cancelId: 99,
-        message:
-          'There is an update available. Would you like to update CoinWatch now?',
-      },
-      response => {
-        console.log(`Exit: ${response}`); // eslint-disable-line no-console
-
-        if (response === 0) {
-          this.setQuitState(true);
-          autoUpdater.quitAndInstall();
-        }
-      },
-    );
   }
 }
 
