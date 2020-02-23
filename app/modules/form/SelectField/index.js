1ea0ab61d40f227e4ab45e92b5d66bb66343c3c2
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2020-02-23 14:41:31 +0000

    Upgrade react-select

--- app/modules/form/SelectField/index.js
+++ app/modules/form/SelectField/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Select from 'react-select';
-import 'react-select/dist/react-select.css';
 import PropTypes from 'prop-types';
 
-const SelectField = (props) => (
-  <Select
-    {...props}
-    value={props.input.value}
-    onChange={(value) => props.input.onChange(value)}
-    onBlur={() => props.input.onBlur(props.input.value)}
-    options={props.options}
-  />
-);
+const SelectField = (props) => {
+  return (
+    <Select
+      {...props}
+      value={props.input.value}
+      onChange={(value) => props.input.onChange(value)}
+      onBlur={() => props.input.onBlur(props.input.value)}
+      options={props.options}
+    />
+  );
+}
 
 SelectField.propTypes = {
   input: PropTypes.shape({
